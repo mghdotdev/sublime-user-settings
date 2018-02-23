5c9e73e057125a5c96fe54e4c8edb02a8e0ef21c
Max Hegler <max@maxhegler.com>
2018-02-23 09:48:18 -0800

    update

--- DefaultSyntax.py
+++ DefaultSyntax.py
@@ -2,6 +2,6 @@ import sublime, sublime_plugin
 
 class DefaultSyntax(sublime_plugin.EventListener):
 	def on_new(self, view):
-		setting = view.settings().get('default_syntax', 'Packages/Text/Plain text.tmLanguage')
+		setting = view.settings().get('default_syntax', 'Packages/Text/Plain text.sublime-syntax')
 		setting = sublime.active_window().active_view().settings().get('default_syntax', setting)
 		view.set_syntax_file(setting)
